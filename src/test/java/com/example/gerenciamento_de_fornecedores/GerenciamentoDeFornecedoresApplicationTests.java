9db894f50ffb007365fab358a6c66e39242e1405
Arthur L Molina <arthurlavagnollimolina@gmail.com>
2026-03-20 13:40:47 -0300

    Initial commit

---
+++ src/test/java/com/example/gerenciamento_de_fornecedores/GerenciamentoDeFornecedoresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gerenciamento_de_fornecedores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoDeFornecedoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
